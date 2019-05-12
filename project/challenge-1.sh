8b42dcf31617a255a72bdb0dd8a3a27667077b6c
connor cahill <connorcahill913@gmail.com>
2019-05-12 22:03:29 +0000

    [Adds]: exact match to grep

--- project/challenge-1.sh
+++ project/challenge-1.sh
@@ -2,6 +2,6 @@
 
 NUMBER=$1
 
-grep "${NUMBER:0:7}" $2
+grep -w "^${NUMBER:0:7}" $2
 if (($?)); then grep -w "${NUMBER:0:5}" $2;fi
 if (($?)); then echo "Phone number prefix not found in data set.";fi
